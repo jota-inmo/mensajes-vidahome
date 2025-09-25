0165f94190bc01d893100998dcebb3afa8721392
jota-inmo <Jc@vidahome.es>
2025-09-25 18:42:43 +0200

    feat: Add favicon and improve icon rendering
    Adds a favicon to the application for better branding and includes an SVG representation of the WhatsApp logo. Also, refactors the LogoIcon component for cleaner rendering of the WhatsApp icon.

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export const PlusIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
@@ -20,12 +19,12 @@ export const WhatsAppIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => 
 );
 
 export const LogoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-      {/* Chat bubble outline */}
-      <path strokeLinecap="round" strokeLinejoin="round" d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v10z" />
-      {/* House outline inside */}
-      <path strokeLinecap="round" strokeLinejoin="round" d="M8 11.5l4-3 4 3M8 11.5v3h8v-3" />
-    </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
+    {/* WhatsApp-style bubble shape (filled) */}
+    <path d="M12.395 2C6.913 2 2.5 6.364 2.5 11.72c0 1.944.53 3.753 1.484 5.32l-1.52 5.564 5.76-1.503c1.52.868 3.27 1.39 5.17 1.39 5.482 0 9.896-4.364 9.896-9.72s-4.414-9.72-9.896-9.72z" />
+    {/* VidaHome house icon inside (stroked, white) */}
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9.5 15.5h5v-4l-2.5-2-2.5 2v4z" stroke="#FFF" strokeWidth={1.2} fill="none" />
+  </svg>
 );
 
 export const SparklesIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
@@ -75,6 +74,16 @@ export const ChevronDownIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) 
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
   </svg>
+);
+
+export const GoogleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" {...props}>
+        <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
+        <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
+        <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z" fill="#FBBC05"/>
+        <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
+        <path d="M1 1h22v22H1z" fill="none"/>
+    </svg>
 );
 
 export const UserCircleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
